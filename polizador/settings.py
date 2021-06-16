1fdbe18c417b3176b24f3e0e565536db8753d2b0
willylobo@gmail.com <willylobo@gmail.com>
2021-06-16 19:27:04 -0300

    Decoupled settings.py

--- polizador/settings.py
+++ polizador/settings.py
@@ -12,25 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.utils import translation
-
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET")
-with open("/var/www/Polizador-v3.0/polizador/SECRET.TXT") as f:
-	SECRET_KEY = f.read().strip()
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["willylobo.net.ar"]
-
 
 # Application definition
 
@@ -81,21 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'polizador.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-with open("/var/www/Polizador-v3.0/polizador/DBSECRET.TXT") as s:
-        DBSECRET = s.read().strip()
-DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.mysql",
-            "HOST": "127.0.0.1",
-            "USER": "willy",
-	    "PASSWORD":DBSECRET,
-            "NAME":"polizadordb",
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
